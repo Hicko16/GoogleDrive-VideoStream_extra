773cd76c92fbeb3b78db0149398ebd112197398f
Dylan Durdle <ddurdle@gmail.com>
2018-07-17 22:42:49 -0400

    testing

--- emby helpers/clone_user.pl
+++ emby helpers/clone_user.pl
@@ -34,14 +34,14 @@ if (-e $targetDirectory . '/userdata/' . $userTarget . '/'){
 }
 
 # make user
-mkdir "$targetDirectory/userdata/$userTarget/" or die("cannot create ". "$targetDirectory/userdata/$userTarget/");
-mkdir "$targetDirectory/users/$userTarget/" or die("cannot create ".  "$targetDirectory/users/$userTarget/");
+mkdir "\"$targetDirectory/userdata/$userTarget/\"" or die("cannot create ". "$targetDirectory/userdata/$userTarget/");
+mkdir "\"$targetDirectory/users/$userTarget/\"" or die("cannot create ".  "$targetDirectory/users/$userTarget/");
 
-copy("$sourceDirectory/userdata/$userSource/displayprefs.json", "$targetDirectory/userdata/$userTarget/displayprefs.json") or die ("cannot copy displayprefs.json " . "$sourceDirectory/userdata/$userSource/displayprefs.json ". "$targetDirectory/userdata/$userTarget/displayprefs.json");
-copy("$sourceDirectory/userdata/$userSource/userdata.json", "$targetDirectory/userdata/$userTarget/userdata.json") or die ("cannot copy userdata.json");
-copy("$sourceDirectory/users/$userSource/sec.txt", "$targetDirectory/users/$userTarget/sec.txt")  or die ("cannot copy sec.txt");
-copy("$sourceDirectory/users/$userSource/config.xml", "$targetDirectory/users/$userTarget/config.xml") or die ("cannot copy config.xml");
-copy("$sourceDirectory/users/$userSource/policy.xml", "$targetDirectory/users/$userTarget/policy.xml") or die ("cannot copy policy.xml");
+copy("\"$sourceDirectory/userdata/$userSource/displayprefs.json\"", "\"$targetDirectory/userdata/$userTarget/displayprefs.json\"") or die ("cannot copy displayprefs.json " . "$sourceDirectory/userdata/$userSource/displayprefs.json ". "$targetDirectory/userdata/$userTarget/displayprefs.json");
+copy("\"$sourceDirectory/userdata/$userSource/userdata.json\"", "\"$targetDirectory/userdata/$userTarget/userdata.json\"") or die ("cannot copy userdata.json");
+copy("\"$sourceDirectory/users/$userSource/sec.txt\"", "\"$targetDirectory/users/$userTarget/sec.txt\"")  or die ("cannot copy sec.txt");
+copy("\"$sourceDirectory/users/$userSource/config.xml\"", "\"$targetDirectory/users/$userTarget/config.xml\"") or die ("cannot copy config.xml");
+copy("\"$sourceDirectory/users/$userSource/policy.xml\"", "\"$targetDirectory/users/$userTarget/policy.xml\"") or die ("cannot copy policy.xml");
 
 print "created " . $userTarget . "\n";
 
