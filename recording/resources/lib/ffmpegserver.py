52ff5bb9b9146394e71093a1087fd2f578fcdca3
Dylan Durdle <ddurdle@gmail.com>
2018-08-30 13:08:01 -0400

    fix path

--- recording/resources/lib/ffmpegserver.py
+++ recording/resources/lib/ffmpegserver.py
@@ -69,9 +69,9 @@ class ffmpegServer(BaseHTTPRequestHandler):
 
                 print "command = " + str(cmd) + "\n"
                 print "path = " + str(path) + "\n"
-                if not os.path.exists(os.path.dirname(path)):
+                if not os.path.exists(path):
                     try:
-                        os.makedirs(os.path.dirname(path))
+                        os.makedirs(path)
                     except OSError as exc: # Guard against race condition
                         if exc.errno != errno.EEXIST:
                             raise
