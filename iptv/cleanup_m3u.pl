a3724d388a58d5c7e7846e3c52b86e3047a12ee4
Dylan Durdle <ddurdle@gmail.com>
2018-09-09 09:23:59 -0400

    finalize changes

--- iptv/cleanup_m3u.pl
+++ iptv/cleanup_m3u.pl
@@ -10,18 +10,20 @@ use constant RETRY => 2;
 
 use Getopt::Std;		# and the getopt module
 
-use constant USAGE => $0 . ' -s  source.tab -t target.m3u8';
+use constant USAGE => $0 . ' -s  source.tab -t target.m3u8 -a service_number';
 
 
 use IO::Handle;
 
 my %opt;
-die (USAGE) unless (getopts ('s:t:c:',\%opt));
+die (USAGE) unless (getopts ('s:t:c:a:',\%opt));
 
 # directory to scan
 my $source = $opt{'s'};
 my $target = $opt{'t'};
 my $channelList = $opt{'c'};
+my $serviceNumber = $opt{'a'};
+
 
 die(USAGE) if ($source eq '' or $target eq '');
 
@@ -32,20 +34,49 @@ open (CHANNELS, $channelList) or die ("cannot open $channelList: " + $!);
 my %channelMapping;
 while (my $line = <CHANNELS>){
 
-	 $line =~ s%\r%%;
+	$line =~ s%\r?\n%%;
 	if ($line =~ m%^([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)%){
 		my ($channelNumber,$country,$channelName,$cleanChannelName) = $line =~  m%^([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)%;
 		$channelMapping{$country . ' - ' . $channelName}[0] = $channelNumber;
 		$channelMapping{$country . ' - ' . $channelName}[1] = $cleanChannelName;
-		print "$channelNumber $country $channelName\n";
+		#print $channelMapping{$country . ' - ' . $channelName}[1] ." $country ${channelName}x\n";
 	}
 
 
 }
+close(CHANNELS);
 open (INPUT, $source) or die ("cannot open $source: " + $!);
 open (OUTPUT, '> '.$target) or die ("cannot create $target: " + $!);
 OUTPUT->autoflush;
+my $line = <INPUT>;
+$line =~ s%\r%%;
+print OUTPUT $line;
+while (my $line = <INPUT>){
 
+	#remove carriage return
+	$line =~ s%\r?\n%%;
+
+	if ($line =~ m%^\#EXTINF\:\-1%){
+		($country,$channel) = $line =~ m%^\#EXTINF\:\-1\,([^\:]+)\: ([^\n]+)%;
+		$channel =~ s%\{[^\}]+\}%%;
+		$channel =~ s%\([^\)]+\)%%;
+		$channel =~ s%\[[^\)]+\]%%;
+		$channel =~ s% \- %%;
+		$channel =~ s%\s+$%%;
+
+		if (defined($channelMapping{$country . ' - ' . $channel}[0])){
+			print OUTPUT "#EXTINF:-1 tvg-id=\"".$channelMapping{$country . ' - ' . $channel}[0].$serviceNumber."\" tvg-name=\"".$channelMapping{$country . ' - ' . $channel}[1]."\"\n";
+			my $line = <INPUT>;
+			$line =~ s%\r%%;
+			print OUTPUT $line . "\n";
+			#print "$country ${channel}x\n";
+		}else{
+			print "$country $channel not defined\n";
+		}
+	}
+
+
+}
 
 close(OUTPUT);
 close(INPUT);
