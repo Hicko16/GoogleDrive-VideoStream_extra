67d34e5110f0948cddce0ea8a8d1bfef7b6997af
Dylan Durdle <ddurdle@gmail.com>
2018-09-08 20:04:42 -0400

    add cleanup m3u8.pl

--- iptv/cleanup_m3u.pl
+++ iptv/cleanup_m3u.pl
@@ -16,11 +16,12 @@ use constant USAGE => $0 . ' -s  source.tab -t target.m3u8';
 use IO::Handle;
 
 my %opt;
-die (USAGE) unless (getopts ('s:t:',\%opt));
+die (USAGE) unless (getopts ('s:t:c:',\%opt));
 
 # directory to scan
 my $source = $opt{'s'};
 my $target = $opt{'t'};
+my $channelList = $opt{'c'};
 
 die(USAGE) if ($source eq '' or $target eq '');
 
@@ -30,16 +31,14 @@ open (INPUT, $source) or die ("cannot open $source: " + $!);
 open (OUTPUT, '> '.$target) or die ("cannot create $target: " + $!);
 OUTPUT->autoflush;
 
-my $channel = '';
-my $country = '';
-my $type = '';
+my %channelMapping;
 while (my $line = <INPUT>){
 
 	 $line =~ s%\r%%;
 	if ($line =~ m%^[^\t]+\t1%){
-		($channelNumber,$country,$channelName,$channelURL) = $line =~  m%^([^\t]+)\t1\t([^\t]+)\t([^\t]+)\t[^\t]*\t([^\t]+)%;
-		print OUTPUT "#EXTINF:-1 tvg-id=\"$channelNumber\" tvg-name=\"$channelName\"\n";
-		print OUTPUT $channelURL . "\n";
+		my ($channelNumber,$country,$channelName,$cleanChannelName) = $line =~  m%^([^\t]+)\t([^\t]+)\t([^\t]+)\t([^\t]+)%;
+		$channelMapping{$country . ' - ' . $channelName}[0] = $channelNumber;
+		$channelMapping{$country . ' - ' . $channelName}[1] = $cleanChannelName;
 		print "$channelNumber $country $channelName\n";
 	}
 
