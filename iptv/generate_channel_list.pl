dacab2723e3e1c9d40ec31da7f942974ba9a0bf7
Dylan Durdle <ddurdle@gmail.com>
2018-09-19 22:14:08 -0400

    add iptvproxy

---
+++ iptv/generate_channel_list.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/perl
+
+###
+##
+## The purpose of this script is to remove invalid IPTV channels in a M3U file.
+## The script takes a provided M3U file (-s) and outputs a copy of the claned-up M3U file (-t).
+## The script takes a provided comma delimited list (-l).
+###
+# number of times to retry when ffmpeg encounters network errors
+use constant RETRY => 2;
+use constant WEB_TEST => 0;
+
+use Getopt::Std;		# and the getopt module
+
+use constant USAGE => $0 . ' -s source.m3u8 -t target.m3u8 (-l list1,list2)';
+
+
+
+use LWP::UserAgent;
+use LWP;
+use IO::Handle;
+
+my %opt;
+die (USAGE) unless (getopts ('s:t:l:',\%opt));
+
+# directory to scan
+my $source = $opt{'s'};
+my $target = $opt{'t'};
+
+die(USAGE) if ($source eq '' or $target eq '');
+
+my $list = $opt{'l'};
+my @filter;
+
+while (my ($item) = $list =~ m%^([^\,]+)%){
+	$list =~ s%^[^\,]+\,?%%;
+	print "item = $item\n";
+	push(@filter,$item);
+
+}
+
+
+ my $ua = new LWP::UserAgent;	# call the constructor method for this object
+
+$ua->agent('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)');		# set the identity
+$ua->timeout(30);		# set the timeout
+
+
+open (INPUT, $source) or die ("cannot open $source: " + $!);
+open (OUTPUT, '> '.$target) or die ("cannot create $target: " + $!);
+OUTPUT->autoflush;
+my $line = <INPUT>;
+
+my $buffer = '';
+my $isSuccess = 0;
+
+my $channel = '';
+my $originalChannel = '';
+my $country = '';
+my $type = '';
+my $skip=0;
+while (my $line = <INPUT>){
+
+	$line =~ s%\r%%;
+	if ($skip){
+		$skip = 0;
+		next;
+	}
+	if ($line =~ m%^\#EXTINF\:\-1%){
+		($country,$channel) = $line =~ m%^\#EXTINF\:\-1\,([^\:]+)\: ([^\n]+)%;
+		$originalChannel = $channel;
+		$channel =~ s%\{[^\}]+\}%%;
+		$channel =~ s%\([^\)]+\)%%;
+		$channel =~ s%\[[^\)]+\]%%;
+		$channel =~ s% \- %%;
+
+		if ($channel =~ m%news%i or $channel =~ m%%i){
+			$type = 'news';
+		}elsif($channel =~ m%sport%i or $channel =~ m%espn%i or $channel =~ m%nfl% or $channel =~ m%nba%i or $channel =~ m%nhl%){
+			$type = 'sports';
+		}elsif($channel =~ m%stars%i or $channel =~ m%showtime%i){
+			$type = 'movies';
+		}else{
+			$type = '';
+		}
+
+		print "$country $channel $type\n";
+	}
+
+	if ($line =~ m%^\#%){
+		next;
+	}
+	print OUTPUT "$originalChannel\t$country\t$channel\t$type\n";
+
+}
+close(OUTPUT);
+close(INPUT);
+
+
+
+
+1;
+
+
---
+++ iptv/resources/lib/webproxy.py
@@ -0,0 +1,157 @@
+'''
+    Copyright (C) 2014-2017 ddurdle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+
+
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+from SocketServer import ThreadingMixIn
+import threading
+import re
+import urllib, urllib2
+import sys
+
+
+class ThreadedWebGUIServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+class WebProxyServer(ThreadingMixIn,HTTPServer):
+
+    def __init__(self, *args, **kw):
+        HTTPServer.__init__(self, *args, **kw)
+        self.ready = True
+
+
+
+class webProxy(BaseHTTPRequestHandler):
+
+
+    #Handler for the GET requests
+    def do_POST(self):
+
+        # debug - print headers in log
+        headers = str(self.headers)
+        print(headers)
+
+
+        print "POST\n\n"
+
+        # passed a kill signal?
+        if self.path == '/kill':
+#            self.server.ready = False
+            return
+
+
+
+
+
+    def do_HEAD(self):
+
+        # debug - print headers in log
+        headers = str(self.headers)
+        print(headers)
+
+
+        print "HEAD HEAD HEAD\n\n"
+
+        # passed a kill signal?
+        if self.path == '/kill':
+#            self.server.ready = False
+            return
+
+
+
+
+    #Handler for the GET requests
+    def do_GET(self):
+
+
+
+        # debug - print headers in log
+        headers = str(self.headers)
+        print(headers)
+
+
+        # passed a kill signal?
+        if self.path == '/kill':
+            self.send_response(200)
+            self.end_headers()
+            if self.server.username is not None:
+                self.wfile.write('<html><form action="/kill" method="post">Username: <input type="text" name="username"><br />Password: <input type="password" name="password"><br /><input type="submit" value="Stop Server"></form></html>')
+            else:
+                self.wfile.write('<html><form action="/kill" method="post"><input type="submit" value="Stop Server"></form></html>')
+
+            #self.server.ready = False
+            return
+
+
+        # redirect url to output
+        elif re.search(r'/twisted/', str(self.path)):
+#            self.send_response(200)
+#            self.end_headers()
+            print "TWISTED" + "\n\n\n"
+            count = 0
+            results = re.search(r'/([^\/]+)/(.*)$', str(self.path))
+            if results:
+                domain = str(results.group(1))
+                parameters = str(results.group(2))
+            #self.send_response(200)
+            #self.end_headers()
+            #xbmcplugin.assignOutputBuffer(self.wfile)
+            #cookies = self.headers['Cookie']
+
+            req = urllib2.Request(domain + '/' + parameters,  None)
+            print "proxy "+ domain + '/' + parameters + "\n"
+
+            try:
+                response = urllib2.urlopen(req)
+            except urllib2.URLError, e:
+                if e.code == 403 or e.code == 401:
+                    print "STILL ERROR"+str(e.code)+"\n"
+                    return
+                else:
+                    return
+
+            self.send_response(200)
+
+            print str(response.info()) + "\n"
+            self.send_header('Content-Type',response.info().getheader('Content-Type'))
+            #self.send_header('Content-Range', response.info().getheader('Content-Range'))
+            self.send_header('Cache-Control',response.info().getheader('Cache-Control'))
+            self.send_header('Date',response.info().getheader('Date'))
+            #self.send_header('Content-type','video/mp4')
+            #self.send_header('Accept-Ranges','bytes')
+
+            self.end_headers()
+
+            CHUNK = 16 * 1024
+            while True:
+                chunk = response.read(CHUNK)
+                if not chunk:
+                    break
+                self.wfile.write(chunk)
+
+            #response_data = response.read()
+            response.close()
+            return
+
+
+        # redirect url to output
+        else:
+            # no options
+            return
