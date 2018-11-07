78847335c2dd32ffb20be4584c594b215f8be643
Dylan Durdle <ddurdle@gmail.com>
2018-11-07 18:57:02 -0500

    updated videostream monitor

--- emby helpers/monitor_videostream.pl
+++ emby helpers/monitor_videostream.pl
@@ -18,15 +18,23 @@ use lib dirname (__FILE__) ;
 #require 'crawler.pm';
 
 require '../crawler.pm';
+use constant USAGE => $0 . " -i emby-server -p 8096 -l label [-w webhook] [-t]\n\t -t sends test message\n";
 
 
 my %opt;
-die (USAGE) unless (getopts ('w:p:d:',\%opt));
+die (USAGE) unless (getopts ('w:p:d:l:t',\%opt));
 
 my $port  = $opt{'p'};
 my $directory  = $opt{'d'};
 my $url = 'http://localhost:'.$port;
 my $webhook = $opt{'w'};
+my $isTest=0;
+$isTest = 1 if ($opt{'t'});
+my $processName = 'python default.py';
+
+
+die(USAGE) if ($port eq '' or $directory eq '');
+
 
 TOOLS_CRAWLER::ignoreCookies();
 my @results = TOOLS_CRAWLER::simpleGET($url);
@@ -35,10 +43,15 @@ if ($results[0] != 1){
 	sleep 30;
 	@results = TOOLS_CRAWLER::simpleGET($url);
 	if ($results[0] != 1){
-		`cd "$directory";sh vs-server.sh restart; sh vs-server.sh start`;
-		if ($webhook ne ''){
-        	`curl -X POST --data '{ "embeds": [{"title": "VideoStream Issue", "description": "Instance restarted - missing process", "type": "link" }] }' -H "Content-Type: application/json" $webhook`;
+		#`cd "$directory";sh vs-server.sh restart; sh vs-server.sh start`;
+
+		my $pid = `ps -ef | grep '$processName' | grep -v grep | awk '{print \$2}'`;
+		while(my ($line) = $pid =~ m%(\d+)\n%){
+        	$pid =~ s%\d+\n%%;
+        	`kill -9 $pid`;
 		}
+		`cd "$directory";nohup $processName &`;
+      	`curl -X POST --data '{ "embeds": [{"title": "VideoStream Issue", "description": "$label -- Instance restarted - missing process", "type": "link" }] }' -H "Content-Type: application/json" $webhook` if ($webhook ne '');
 
 	}
 
@@ -46,5 +59,10 @@ if ($results[0] != 1){
 
 
 
+if ($isTest){
+
+      	`curl -X POST --data '{ "embeds": [{"title": "VideoStream Issue", "description": "$label -- TEST MESSAGE", "type": "link" }] }' -H "Content-Type: application/json" $webhook` if ($webhook ne '');
+
+}
 
 
