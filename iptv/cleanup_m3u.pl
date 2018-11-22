5fcf6c5595ba60695aea2dd7b1f798d5752b913b
Dylan Durdle <ddurdle@gmail.com>
2018-11-22 12:16:38 -0500

    exclude duplicates logic

--- iptv/cleanup_m3u.pl
+++ iptv/cleanup_m3u.pl
@@ -10,13 +10,13 @@ use constant RETRY => 2;
 
 use Getopt::Std;		# and the getopt module
 
-use constant USAGE => $0 . ' -s  source.m3u8 -t target.m3u8 -a service_number (-c channellist.tab | -n number)';
+use constant USAGE => $0 . ' -s  source.m3u8 -t target.m3u8 -a service_number (-c channellist.tab | -n number) -l\n -l limits to 1 channel occurrence';
 
 
 use IO::Handle;
 
 my %opt;
-die (USAGE) unless (getopts ('s:t:c:a:n:',\%opt));
+die (USAGE) unless (getopts ('s:t:c:a:n:l',\%opt));
 
 # directory to scan
 my $source = $opt{'s'};
@@ -24,6 +24,8 @@ my $target = $opt{'t'};
 my $channelList = $opt{'c'};
 my $serviceNumber = $opt{'a'};
 my $number = $opt{'n'};
+my $limit = 1 if defined ($opt{'l'});
+
 
 my @channelcache;
 
@@ -82,15 +84,20 @@ while (my $line = <INPUT>){
 		if (defined($channelMapping{$country . ' - ' . $channel}[0])){
 			my $channelNumber = int($channelMapping{$country . ' - ' . $channel}[0].$serviceNumber);
 			print $channelNumber . " x\n";
-			while ($channelcache[$channelNumber] != 0){
-				$channelNumber++;
+			if ($channelcache[$channelNumber] != 0 and $limit){
+				print "|$country|${channel}|".$channelMapping{$country . ' - ' . $channel}[0]."| duplicate\n";
+			}else{
+				while ($channelcache[$channelNumber] != 0){
+					$channelNumber++;
+				}
+				$channelcache[$channelNumber]++;
+				print OUTPUT "#EXTINF:-1 tvg-id=\"".$channelNumber."\" tvg-name=\"".$channelMapping{$country . ' - ' . $channel}[1]."\"\n";
+				my $line = <INPUT>;
+				$line =~ s%\r%%;
+				print OUTPUT $line . "\n";
+				print "$channelNumber $country ${channel}x\n";
 			}
-			$channelcache[$channelNumber]++;
-			print OUTPUT "#EXTINF:-1 tvg-id=\"".$channelNumber."\" tvg-name=\"".$channelMapping{$country . ' - ' . $channel}[1]."\"\n";
-			my $line = <INPUT>;
-			$line =~ s%\r%%;
-			print OUTPUT $line . "\n";
-			print "$channelNumber $country ${channel}x\n";
+
 		}elsif ($number ne ''){
 			($channel) = $line =~ m%^\#EXTINF\:\-1\,([^\n]+)$%;
 			print OUTPUT "#EXTINF:-1 tvg-id=\"".$number.$serviceNumber."\" tvg-name=\"".$channel."\"\n";
