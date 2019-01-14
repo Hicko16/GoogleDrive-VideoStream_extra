3f18cc2f4fc0b45ebeab40fbebc5e6607302d7a5
Dylan Durdle <ddurdle@gmail.com>
2019-01-14 10:44:04 -0500

    add a plex backup script

---
+++ plex_helpers/backup.sh
@@ -0,0 +1,12 @@
+tar -zcf Cache.$1.tgz Cache
+tar -zcf Codecs.$1.tgz Codecs
+tar -zcf "Crash Reports.$1.tgz" "Crash Reports"
+tar -zcf Diagnostics.$1.tgz Diagnostics
+tar -zcf Logs.$1.tgz Logs
+tar -zcf Media.$1.tgz Media
+tar -zcf Metadata.$1.tgz Metadata
+tar -zcf Plug-ins.$1.tgz Plug-ins
+service plexmediaserver stop
+sleep 5
+tar -zcf "Plug-in Support.$1.tgz" "Plug-in Support"
+service plexmediaserver start
