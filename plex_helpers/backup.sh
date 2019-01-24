5c5fe7664e2993ebfd45547c08550bfb487955b1
Dylan Durdle <ddurdle@gmail.com>
2019-01-24 11:27:20 -0500

    fix date

--- plex_helpers/backup.sh
+++ plex_helpers/backup.sh
@@ -3,14 +3,14 @@
 #
 date=`date --reference=last.backup.done`
 touch last.backup.start
-tar -N $date -zcf Cache.$1.tgz Cache
-tar -N $date -zcf Codecs.$1.tgz Codecs
-tar -N $date -zcf "Crash Reports.$1.tgz" "Crash Reports"
-tar -N $date -zcf Diagnostics.$1.tgz Diagnostics
-tar -N $date -zcf Logs.$1.tgz Logs
-tar -N $date -zcf Media.$1.tgz Media
-tar -N $date -zcf Metadata.$1.tgz Metadata
-tar -N $date -zcf Plug-ins.$1.tgz Plug-ins
+tar -N "$date" -zcf Cache.$1.tgz Cache
+tar -N "$date" -zcf Codecs.$1.tgz Codecs
+tar -N "$date" -zcf "Crash Reports.$1.tgz" "Crash Reports"
+tar -N "$date" -zcf Diagnostics.$1.tgz Diagnostics
+tar -N "$date" -zcf Logs.$1.tgz Logs
+tar -N "$date" -zcf Media.$1.tgz Media
+tar -N "$date" -zcf Metadata.$1.tgz Metadata
+tar -N "$date" -zcf Plug-ins.$1.tgz Plug-ins
 service plexmediaserver stop
 sleep 5
 tar -zcf "Plug-in Support.$1.tgz" "Plug-in Support"
