d9f62a3c3c67ab41af30f3e8a11d43aa71f183c5
Dylan Durdle <ddurdle@gmail.com>
2019-01-24 11:21:56 -0500

    add new date logic tracking

--- plex_helpers/backup.sh
+++ plex_helpers/backup.sh
@@ -1,16 +1,19 @@
 #
-# backup.sh current_date_YYYYMMDD "'last_backup_date_time_YYYY:MM:DD hh:mm:ss'"
+# backup.sh current_date_YYYYMMDD
 #
-tar -N $2 -zcf Cache.$1.tgz Cache
-tar -N $2 -zcf Codecs.$1.tgz Codecs
-tar -N $2 -zcf "Crash Reports.$1.tgz" "Crash Reports"
-tar -N $2 -zcf Diagnostics.$1.tgz Diagnostics
-tar -N $2 -zcf Logs.$1.tgz Logs
-tar -N $2 -zcf Media.$1.tgz Media
-tar -N $2 -zcf Metadata.$1.tgz Metadata
-tar -N $2 -zcf Plug-ins.$1.tgz Plug-ins
+date=`date --reference=last.backup.done`
+touch last.backup.start
+tar -N $date -zcf Cache.$1.tgz Cache
+tar -N $date -zcf Codecs.$1.tgz Codecs
+tar -N $date -zcf "Crash Reports.$1.tgz" "Crash Reports"
+tar -N $date -zcf Diagnostics.$1.tgz Diagnostics
+tar -N $date -zcf Logs.$1.tgz Logs
+tar -N $date -zcf Media.$1.tgz Media
+tar -N $date -zcf Metadata.$1.tgz Metadata
+tar -N $date -zcf Plug-ins.$1.tgz Plug-ins
 service plexmediaserver stop
 sleep 5
 tar -zcf "Plug-in Support.$1.tgz" "Plug-in Support"
 cp Preferences.xml Preferences.$1.xml
 service plexmediaserver start
+mv last.backup.start last.backup.done
