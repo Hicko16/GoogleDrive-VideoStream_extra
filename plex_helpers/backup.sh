c4aad4406af0d5c7563eb1efab4f6a01e35f64dc
Dylan Durdle <ddurdle@gmail.com>
2019-01-24 11:08:30 -0500

    fix date format

--- plex_helpers/backup.sh
+++ plex_helpers/backup.sh
@@ -1,14 +1,14 @@
 #
-# backup.sh current_date_YYYYMMDD last_backup_date_time_YYYY:MM:DD hh:mm:ss
+# backup.sh current_date_YYYYMMDD "'last_backup_date_time_YYYY:MM:DD hh:mm:ss'"
 #
-tar -N '$2' -zcf Cache.$1.tgz Cache
-tar -N '$2' -zcf Codecs.$1.tgz Codecs
-tar -N '$2' -zcf "Crash Reports.$1.tgz" "Crash Reports"
-tar -N '$2' -zcf Diagnostics.$1.tgz Diagnostics
-tar -N '$2' -zcf Logs.$1.tgz Logs
-tar -N '$2' -zcf Media.$1.tgz Media
-tar -N '$2' -zcf Metadata.$1.tgz Metadata
-tar -N '$2' -zcf Plug-ins.$1.tgz Plug-ins
+tar -N $2 -zcf Cache.$1.tgz Cache
+tar -N $2 -zcf Codecs.$1.tgz Codecs
+tar -N $2 -zcf "Crash Reports.$1.tgz" "Crash Reports"
+tar -N $2 -zcf Diagnostics.$1.tgz Diagnostics
+tar -N $2 -zcf Logs.$1.tgz Logs
+tar -N $2 -zcf Media.$1.tgz Media
+tar -N $2 -zcf Metadata.$1.tgz Metadata
+tar -N $2 -zcf Plug-ins.$1.tgz Plug-ins
 service plexmediaserver stop
 sleep 5
 tar -zcf "Plug-in Support.$1.tgz" "Plug-in Support"
