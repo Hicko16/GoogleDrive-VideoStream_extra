f46f30328497f906316a03b695b1cf9d18c73d7c
Dylan Durdle <ddurdle@gmail.com>
2019-02-21 12:26:19 -0500

    addnew scheduled job maker

---
+++ emby helpers/library_scan.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+###
+##
+## The purpose of this script is to shutdown emby cleanly, and back up the library file.
+##
+##
+###
+
+use Getopt::Std;		# and the getopt module
+use File::Copy;
+
+use File::Basename;
+use lib dirname (__FILE__) ;
+require '../crawler.pm';
+
+use constant USAGE => $0 . "-p 8096 -a api_key -i id";
+
+
+
+my %opt;
+die (USAGE) unless (getopts ('p:a:i:',\%opt));
+
+my $id  = $opt{'i'};
+my $port =  $opt{'p'};
+my $apiKey = $opt{'a'};
+
+die(USAGE) if ($port eq '' or $id eq '');
+
+
+my $url = 'http://127.0.0.1:'.$port.'/emby/ScheduledTasks/Running/6330ee8fb4a957f33981f89aa78b030f?api_key='.$apiKey;
+
+TOOLS_CRAWLER::ignoreCookies();
+my @results = TOOLS_CRAWLER::simplePOST($url);
+
+
+
+
