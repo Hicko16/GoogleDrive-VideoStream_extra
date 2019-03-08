d106d046be54209d4549b8460ffb8345b685c289
Dylan Durdle <ddurdle@gmail.com>
2019-03-08 18:40:19 -0500

    move the file-copy

--- transcoders/emby_ffmpeg.pl
+++ transcoders/emby_ffmpeg.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 
-use File::Copy qw(move);
-use File::Path qw/make_path/;
 
 
 use File::Basename;
@@ -422,6 +420,8 @@ if ($isSRT){
 
 		for (my $i=0; $i <= $#moveList; $i++){
 			if ($i==0 or $moveList[$i][0] ne $moveList[$i-1][0]){
+use File::Copy qw(move);
+use File::Path qw/make_path/;
 
 				move $moveList[$i][0], $moveList[$i][2];
 				move $moveList[$i][1], $moveList[$i][3];
