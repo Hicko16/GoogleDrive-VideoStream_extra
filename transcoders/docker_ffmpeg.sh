812d414a984cf2109aec249eba547579731b4ce0
Dylan Durdle <ddurdle@gmail.com>
2019-03-11 12:10:38 -0400

    docker ffmpeg should only rediret for gdrive calls

--- transcoders/docker_ffmpeg.sh
+++ transcoders/docker_ffmpeg.sh
@@ -2,6 +2,13 @@
 
 for var in "$@"
 do
-    string="${string} \"$var\""
+	if [[ $string != *"ffmpeg"* ]]; then
+    	string="${string} \"$var\""
+    fi
 done
+if [[ $string == *"9988"* ]]; then
 wget 172.17.0.1:9998/stream --post-data='$string'
+else
+/bin/ffmpeg.oem $string
+fi
+
