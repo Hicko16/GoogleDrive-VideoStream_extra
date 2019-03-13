776c08d7cd984f3342ad7ede13dc7060cd5d9856
Dylan Durdle <ddurdle@gmail.com>
2019-03-13 12:23:37 -0400

    testing

--- transcoders/PlexTranscoder.pl
+++ transcoders/PlexTranscoder.pl
@@ -122,8 +122,8 @@ if ($srtfile ne '' and $seek ne ''){
 
 my $audio;
 if (FORCE_REMUX_AUDIO){
-	$audio = " -i $video";
-	$arglist =~ s%\-codec\:0 aac%\-codec\:1 aac%;
+	$audio = " -i /u01/test.aac";
+	#$arglist =~ s%\-codec\:0 aac%\-codec\:1 aac%;
 
 }else{
 	$audio = '';
