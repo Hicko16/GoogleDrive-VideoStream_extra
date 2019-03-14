c1257ea9b918973104aed2d85cd2b5f778b58b35
Dylan Durdle <ddurdle@gmail.com>
2019-03-14 11:09:43 -0400

    add iptv hook

--- transcoders/docker_ffmpeg.sh
+++ transcoders/docker_ffmpeg.sh
@@ -11,13 +11,19 @@ do
 	  	;;
     esac
 done
+(
+sleep 10000
+echo "killed"
+wget -q 172.17.0.1:9998/stop/$$ --post-data="$string" -O /tmp/$$ &
+) &
 case "$string" in
+	*USERNAME*)
+		wget -q 172.17.0.1:9998/start/$$ --post-data="$string" -O /tmp/$$ &
+        read -n 1 userInput
+        wget -q 172.17.0.1:9998/stop/$$ --post-data="$string" -O /tmp/$$ &
+        echo "killed"
+		;;
 	*998*)
-		(
-		sleep 10000
-		echo "killed"
-		wget -q 172.17.0.1:9998/stop/$$ --post-data="$string" -O /tmp/$$ &
-		) &
 		wget -q 172.17.0.1:9998/start/$$ --post-data="$string" -O /tmp/$$ &
         read -n 1 userInput
         wget -q 172.17.0.1:9998/stop/$$ --post-data="$string" -O /tmp/$$ &
