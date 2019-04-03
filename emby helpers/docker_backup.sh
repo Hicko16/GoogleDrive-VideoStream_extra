90f7038a46b6d2536213dadccdd165d67e922e48
Dylan Durdle <ddurdle@gmail.com>
2019-04-03 09:39:00 -0400

    update backup scripts

--- emby helpers/docker_backup.sh
+++ emby helpers/docker_backup.sh
@@ -1,18 +1,10 @@
 #
-# docker_backup.sh current_date_YYYYMMDD location_of_scripts port access_token docker_instance_name
+# docker_db_backup.sh current_date_YYYYMMDD location_of_scripts port access_token docker_instance_name
 #
-date=`date --reference=last.backup.done`
-sudo touch last.backup.start
-tar -N "$date" -zcf cache.$1.tgz cache
 tar -zcf config.$1.tgz config
-tar -zcf localization.$1.tgz localization
-tar -N "$date" -zcf metadata.$1.tgz metadata
-tar -zcf plugins.$1.tgz plugins
-tar -zcf root.$1.tgz root
-perl $2/stop_emby.pl -p $3 -a $4
+cd "$2"; perl stop_emby.pl -p $3 -a $4; cd -
 sleep 5
 sudo docker stop $5
 sleep 5
 tar -zcf "data.$1.tgz" "data"
 sudo docker start $5
-mv last.backup.start last.backup.done
