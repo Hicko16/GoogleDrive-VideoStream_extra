7d82eef1839245efa04a52b644cc6e8cb7a9f5e6
Dylan Durdle <ddurdle@gmail.com>
2019-04-05 11:54:08 -0400

    add livetv to ffmpeg filter

--- transcoders/docker_ffmpeg.sh
+++ transcoders/docker_ffmpeg.sh
@@ -24,6 +24,17 @@ case "$string" in
         wget -q 172.17.0.1:9999/stop/$$ --post-data="$string" -O /tmp/$$ &
         echo "killed"
 		;;
+	*LiveTv*)
+		(
+		sleep 10000
+		echo "killed"
+		wget -q 172.17.0.1:9999/stop/$$ --post-data="$string" -O /tmp/$$ &
+		) &
+		wget -q 172.17.0.1:9999/start/$$ --post-data="$string" -O /tmp/$$ &
+        read -n 1 userInput
+        wget -q 172.17.0.1:9999/stop/$$ --post-data="$string" -O /tmp/$$ &
+        echo "killed"
+		;;
 	*998*)
 		(
 		sleep 10000
