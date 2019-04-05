223500ed1001512e636309ad5b4bc19febedbdf8
Dylan Durdle <ddurdle@gmail.com>
2019-04-05 14:07:47 -0400

    add back matchcenter

--- iptv/filter_m3u.pl
+++ iptv/filter_m3u.pl
@@ -33,7 +33,7 @@ my $isWebCheck = 1 if defined($opt{'c'});
 my $isRemoveVOD = 1 if defined($opt{'v'});
 
 if ($isRemoveVOD){
-	@blacklist = ('MatchCenter','24/7','²⁴/⁷', 'WEBCAMS', 'mp4', 'XXX', 'null', 'avi', 'mkv');
+	@blacklist = ('24/7','²⁴/⁷', 'WEBCAMS', 'mp4', 'XXX', 'null', 'avi', 'mkv');
 }
 
 
