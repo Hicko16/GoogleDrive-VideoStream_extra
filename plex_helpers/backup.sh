515b64647ca6c024156d36745a4cdd5d9270447f
Dylan Durdle <ddurdle@gmail.com>
2019-04-07 19:12:18 -0400

    updated to incremental

--- plex_helpers/backup.sh
+++ plex_helpers/backup.sh
@@ -3,14 +3,24 @@
 #
 date=`date --reference=last.backup.done`
 touch last.backup.start
+if test -e cache.snar
+then
+cp cache.snar cache.$1.snar
+cp metadata.snar metadata.$1.snar
+MEDIA=media.$1.snar
+METADATA=metadata.$1.snar
+else
+MEDIA=media.snar
+METADATA=metadata.snar
+fi
 #tar -N "$date" -zcf Cache.$1.tgz Cache
-tar -N "$date" -zcf Codecs.$1.tgz Codecs
-tar -N "$date" -zcf "Crash Reports.$1.tgz" "Crash Reports"
-tar -N "$date" -zcf Diagnostics.$1.tgz Diagnostics
-tar -N "$date" -zcf Logs.$1.tgz Logs
-tar -N "$date" -zcf Media.$1.tgz Media
-tar -N "$date" -zcf Metadata.$1.tgz Metadata
-tar -N "$date" -zcf Plug-ins.$1.tgz Plug-ins
+tar -zcf Codecs.$1.tgz Codecs
+#tar -N "$date" -zcf "Crash Reports.$1.tgz" "Crash Reports"
+#tar -N "$date" -zcf Diagnostics.$1.tgz Diagnostics
+#tar -N "$date" -zcf Logs.$1.tgz Logs
+tar -g $MEDIA -zcf Media.$1.tgz Media
+tar -g $METADATA -zcf Metadata.$1.tgz Metadata
+tar -zcf Plug-ins.$1.tgz Plug-ins
 service plexmediaserver stop
 sleep 5
 tar -zcf "Plug-in Support.$1.tgz" "Plug-in Support"
