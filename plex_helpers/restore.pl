69dde0609b2220e8921e8d8751dcd0bc5f212e9f
Dylan Durdle <ddurdle@gmail.com>
2019-04-14 09:55:40 -0400

    add instance variable

--- plex_helpers/restore.pl
+++ plex_helpers/restore.pl
@@ -7,16 +7,17 @@
 
 use Getopt::Std;		# and the getopt module
 
-use constant USAGE => $0 . " -s directory_source_of_backup -t target_directory -d datestamp_YYYMMDD\n";
+use constant USAGE => $0 . " -s directory_source_of_backup -t target_directory -d datestamp_YYYMMDD -i instance\n";
 
 
 my %opt;
-die (USAGE) unless (getopts ('s:t:d:',\%opt));
+die (USAGE) unless (getopts ('s:t:d:i:',\%opt));
 
 # directory to scan
 my $datestamp = $opt{'d'};
 my $backupDirectory = $opt{'s'};
 my $outputDirectory = $opt{'t'};
+my $instance = $opt{'i'};
 
 
 if ($outputDirectory eq ''){
@@ -28,7 +29,10 @@ my @files = glob "'$backupDirectory/*$datestamp*.tgz'";
 
 for (my $i=0; $i <= $#files; $i++){
 	if ($files[$i] =~ m%Plug-in Support%){
+		`docker stop $instance`;
+		sleep(4);
 		`cd '$outputDirectory';mkdir new; cd new; tar -zxf '$files[$i]'`;
+		`docker start $instance`;
 	}else{
 		print STDERR "extract " . $files[$i] . "\n";
 		`cd '$outputDirectory'; tar -zxf '$files[$i]'`;
