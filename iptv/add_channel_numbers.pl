17364c60028eae9aae7fe283987905844ffc77da
Dylan Durdle <ddurdle@gmail.com>
2019-04-15 08:25:38 -0400

    new script

---
+++ iptv/add_channel_numbers.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+###
+##
+## The purpose of this script is to add channel mappings as provided from a TAB containing two columns (map from, map to)
+###
+use Getopt::Std;		# and the getopt module
+
+use constant USAGE => $0 . ' -s target.m3u8 -t target.m3u8 -c mapping.tab';
+
+
+use IO::Handle;
+
+my %opt;
+die (USAGE) unless (getopts ('s:t:c:',\%opt));
+
+# directory to scan
+my $source = $opt{'s'};
+my $target = $opt{'t'};
+my $tab = $opt{'c'};
+
+
+die(USAGE) if ($source eq '' or $target eq '' or $tab eq '');
+
+my %channelMapping;
+open (TAB, $TAB) or die ("cannot open $TAB: " + $!);
+while (my $line = <TAB>){
+	my ($from,$to) = $line =~ m%^(\S+)\s(\S+)%;
+	$channelMapping{$from} = $to;
+
+}
+
+close(TAB);
+
+
+open (INPUT, $source) or die ("cannot open $source: " + $!);
+open (OUTPUT, '> '.$target) or die ("cannot create $target: " + $!);
+OUTPUT->autoflush;
+
+while (my $line = <INPUT>){
+
+	 $line =~ s%\r%%;
+	if ($line =~ m%^#EXTINF%){
+		my $nextLine = <INPUT>;
+		my ($channel) = $nextLine =~ m%\/(\d+)\.m3u8%;
+		$toChannel = $channelMapping{$channel};
+		$line =~ s%\-1%\-1 tvg-id="$toChannel"%;
+		print OUTPUT $line;
+		print OUTPUT $nextLine;
+		print "$line\n";
+	}else{
+		print OUTPUT $line;
+	}
+
+
+}
+close(OUTPUT);
+close(INPUT);
+
+
+
+
+1;
+
+
+
+
+
